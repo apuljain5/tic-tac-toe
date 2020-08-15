f517c067d016cace57fbdbb80a593aa22239a86b
Apul Jain <apuljain5@gmail.com>
2020-08-15 15:45:31 +0530

    added new component 'icon'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconComponent } from './components/icon/icon.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent
   ],
   imports: [
     BrowserModule,
